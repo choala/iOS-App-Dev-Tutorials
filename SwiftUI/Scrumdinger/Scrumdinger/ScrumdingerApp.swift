e1bf605f395468befa9bc70f0e6dfbdb0ac84c5f
Chae-young Park <pcy07111@gmail.com>
2023-08-01 16:14:23 +0900

    feat: Build a project

---
+++ SwiftUI/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 박채영 on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
