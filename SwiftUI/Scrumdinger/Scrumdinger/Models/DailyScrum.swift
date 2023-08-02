390453aec2afc04d7e79e3adf64f730300757e53
Chae-young Park <pcy07111@gmail.com>
2023-08-02 13:51:51 +0900

    feat: Add a computed property for slider value and make a nested structure

--- SwiftUI/Scrumdinger/Scrumdinger/Models/DailyScrum.swift
+++ SwiftUI/Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -7,11 +7,39 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
+    var lengthInMinutesDouble: Double {
+        get {
+            Double(lengthInMinutes)
+        }
+        set {
+            lengthInMinutes = Int(newValue)
+        }
+    }
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees.map { Attendee(name: $0) }
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID = UUID()
+        var name: String
+    }
+    
+    static var emptyScrum: DailyScrum {
+        DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
+    }
 }
 
 extension DailyScrum {
