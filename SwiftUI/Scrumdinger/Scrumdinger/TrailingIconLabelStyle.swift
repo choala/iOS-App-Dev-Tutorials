e22becc1ad7fd43eaa038655e8b323e2a4847a3f
Chae-young Park <pcy07111@gmail.com>
2023-08-02 13:16:21 +0900

    docs: Add some comments for study

--- SwiftUI/Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
+++ SwiftUI/Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// LabelStyle: Label 뷰의 스타일을 커스터마이즈하기 위해 사용되는 프로토콜
 struct TrailingIconLabelStyle: LabelStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
@@ -16,7 +17,8 @@ struct TrailingIconLabelStyle: LabelStyle {
     }
 }
 
-// leading-dot syntax 사용을 위해 static property로 선언
+// Self가 TrailingIconLabelStyle일 때만 사용할 수 있는 정적 변수인 trailingIcon을 추가
 extension LabelStyle where Self == TrailingIconLabelStyle {
+    // leading-dot syntax 사용을 위해 static property로 선언
     static var trailingIcon: Self { Self() }
 }
